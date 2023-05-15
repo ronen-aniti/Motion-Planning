8ae7907501cba11d6a24ce9e1a590cc678bd035f
ronen-aniti <115172276+ronen-aniti@users.noreply.github.com>
2023-05-15 10:23:10 -0400

    Updating documentation

--- Version3/grid.py
+++ Version3/grid.py
@@ -23,7 +23,69 @@ class Grid(Planner):
 		self._goal_grid_east = int(self._goal_state.local_position.east - self._east_offset)
 		self._start_grid = (self._start_grid_north, self._start_grid_east)
 		self._goal_grid = (self._goal_grid_north, self._goal_grid_east)
+		self._build_obstacle_potential_field()
 
+	def _build_obstacle_potential_field(self):
+		altitude = self._goal_state.local_position.down
+		self._north_size = int(self._environment.north_bounds.maximum - self._environment.north_bounds.minimum)
+		self._east_size = int(self._environment.east_bounds.maximum - self._environment.east_bounds.minimum)
+		north_offset = self._environment.obstacles.north_offset
+		east_offset = self._environment.obstacles.east_offset
+		grid = np.zeros((self._north_size, self._east_size))
+		for obstacle in self._environment.obstacles.list:
+			north_start = int(obstacle.north_min - north_offset)
+			north_end = int(obstacle.north_max - north_offset)
+			east_start = int(obstacle.east_min - east_offset)
+			east_end = int(obstacle.east_max - east_offset)
+
+			grid[north_start: north_end, east_start:east_end] = 1.0 # occupied
+
+		obstacle_distance_transform = 100 / scipy.ndimage.distance_transform_edt(1-grid)
+
+
+		plt.imshow(obstacle_distance_transform, origin="lower", cmap="jet")
+		plt.colorbar()
+		plt.show()
+		grid2 = np.zeros((self._north_size, self._east_size))
+		grid2[self._goal_grid_north,self._goal_grid_east] = 1
+		distance_transform_goal = scipy.ndimage.distance_transform_edt(1-grid2)
+
+		plt.imshow(distance_transform_goal, origin="lower", cmap="jet")
+		plt.colorbar()
+		plt.show()
+
+		# Compute the potential field.
+		potential = obstacle_distance_transform + distance_transform_goal
+
+		# Compute the gradient of the potential field.
+		gradient_y, gradient_x = np.gradient(-potential)  # We negate the potential field here because we want to descend the potential field, not ascend it
+
+		# Compute the magnitude of the gradient.
+		gradient_magnitude = np.sqrt(gradient_y**2 + gradient_x**2)
+
+		# Normalize the gradient.
+		gradient_y /= gradient_magnitude
+		gradient_x /= gradient_magnitude
+
+		# Create a grid of coordinates.
+		x, y = np.meshgrid(np.arange(self._east_size), np.arange(self._north_size))
+
+		# Every nth arrow for cleaner plotting
+		nth = 2
+		fig, ax = plt.subplots(figsize=(10, 10))
+
+		# Plot the potential field.
+		ax.imshow(potential, origin="lower", cmap="jet")
+
+		# Plot the vector field.
+		ax.quiver(x[::nth, ::nth], y[::nth, ::nth], gradient_x[::nth, ::nth], gradient_y[::nth, ::nth], scale=50)
+		plt.scatter(self._goal_grid_east, self._goal_grid_north)
+
+		# Show the plot.
+		plt.show()
+
+
+		
 	def _convert_obstacles_to_grid(self):
 		altitude = self._goal_state.local_position.down
 		self._north_size = int(self._environment.north_bounds.maximum - self._environment.north_bounds.minimum)
